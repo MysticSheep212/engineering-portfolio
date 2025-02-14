83cc3b8f2dde533811797d0726a2383aacf56f38
MysticSheep212 <123783386+MysticSheep212@users.noreply.github.com>
2025-02-13 23:32:27 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready');
+    // Add your JavaScript code here
+
+});
+
+
+
+
+// body {
+//     font-family: Arial, sans-serif;
+//     margin: 0;
+//     padding: 0;
+//     background-color: #d5c7ec;
+    
+// }
+// header {
+//     height: 100vh;
+//     background-color: #5a848f;
+//     color: #fff;
+//     padding: 1rem;
+//     text-align: center;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     transform: translateY(-100px); /* Adjust the value as needed */
+// }
