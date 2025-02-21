fbdb21c0e7becc99744c57b8d910f0ac606b0f0a
Dylan <dylan.levoguer@gmail.com>
2025-02-20 18:53:07 -0800

    editing the about.html to merge it with the index.html

--- js/main.js
+++ js/main.js
@@ -5,13 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('nav');
 
     // Makes the navmenu change transparency as you scroll
-    
+
     window.addEventListener('scroll', () => {
         if (window.scrollY > 270) { //change num to change when the navmenu transparency changes
             navbar.classList.add('scrolled');
         } else {
             navbar.classList.remove('scrolled');
         }
+    });
+
+    document.querySelector('nav a[href="#about"]').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.querySelector('#about').scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 
 });
